cf9ea8b71f12535e3bf93406c77575f6e473975e
amakarudze <amakarudze@gmail.com>
2018-05-12 12:04:00 +0200

    Refactor home_page view to use template

--- bookings/views.py
+++ bookings/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def home_page(request):
-    return HttpResponse('<html><title>Book Boardroom</title></html>')
+    return render(request, 'bookings/index.html')
